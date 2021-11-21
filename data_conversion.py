b08dad6d08e2bd23a2b08b93a82d63206c4507ca
Mike Wisniewski <wisniemj@gmail.com>
2021-11-21 15:58:24 -0500

    Added functionality to data_conversion to upload papers

--- data_conversion.py
+++ data_conversion.py
@@ -2,16 +2,6 @@ from pymongo import MongoClient
 import pandas as pd
 import csv
 
-# parameter setting
-connection_string = "mongodb+srv://mwisniewski:nzMIpgjB96hUS2vO@cluster0.817yp.mongodb.net/Cluster0?retryWrites=true&w=majority"
-database_name = "ProjectCS7330"
-authors_collection_name = "Authors"
-
-# connection protocol
-myclient = MongoClient(connection_string)
-mydb = myclient[database_name]
-mycol = mydb[authors_collection_name]
-
 
 def _author_load(filename):
     authors_dict = {"Authors":[]}
@@ -64,10 +54,73 @@ def _author_load(filename):
 
     temp_f.close()
     return authors_dict
+
+def _paper_load(filename):
+    papers_dict = {"Papers":[]}
+    
+    with open(filename, "r") as temp_f:
+        papers = []
+
+        for line in csv.reader(temp_f):
+            paperTitle = line[0]
+            conferenceName = line[1]
+            iteration = line[2]
+            year = line[3]
+            location = line[4]
+
+            # author info
+            firstName_idx = 5
+            lastName_idx = 6
+            affiliation_idx = 7
+            
+            while affiliation_idx <= len(line):
+
+                # check to see if paper current;y exists within database
+                paper_exists = False
+                for key, value in papers_dict.items():
+                    for sub_dict in value:
+                        if sub_dict["Title"] == paperTitle:
+                            sub_dict["Authors"].append(f'{line[firstName_idx]} {line[lastName_idx]}')
+                            paper_exists = True
+
+                # create a new entry if author does not exist
+                if paper_exists == False:
+
+                    papers_dict["Papers"].append(
+                        {
+                            "Title": paperTitle,
+                            "Authors": [f'{line[firstName_idx]} {line[lastName_idx]}'],
+                            "URL": "TESTURL.COM",
+                            "Page Number": "TEST#",
+                            "Publication": [conferenceName]
+                        }
+                    )
+
+                firstName_idx += 3
+                lastName_idx += 3
+                affiliation_idx += 3
+
+    temp_f.close()
+    return papers_dict
                 
 if __name__=="__main__":
-    test_dict = _author_load("16NovExtraction/conferences.csv")
 
-    # upload
-    for author in test_dict["Authors"]:
-        x = mycol.insert_one(author)
+    # parameter setting
+    connection_string = "mongodb+srv://mwisniewski:nzMIpgjB96hUS2vO@cluster0.817yp.mongodb.net/Cluster0?retryWrites=true&w=majority"
+    database_name = "ProjectCS7330"
+    authors_collection_name = "Papers"
+
+    # connection protocol
+    myclient = MongoClient(connection_string)
+    mydb = myclient[database_name]
+    mycol = mydb[authors_collection_name]
+
+    # # upload authors
+    # test_dict = _author_load("16NovExtraction/conferences.csv")
+    # for author in test_dict["Authors"]:
+    #     x = mycol.insert_one(author)
+
+    # upload papers
+    test_dict = _paper_load("16NovExtraction/conferences.csv")
+    for paper in test_dict["Papers"]:
+        x = mycol.insert_one(paper)
